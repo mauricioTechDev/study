6fa0b806f43aa4487118c0b7974373aa48a41eb7
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-02-09 21:57:00 -0500

    Create hash-table-v2.js
    hash-table-v2

---
+++ alumni-bootcamp/practice/hash-table-v2.js
@@ -0,0 +1,45 @@
+class HashTable {
+  constructor(size){
+    this.size = size;
+    this.buckets = Array(this.size)
+
+    for(let i = 0; i < this.buckets.length; i++){
+      this.buckets[i] = new Map()
+    }
+  }
+
+  hash(key, size){
+    let hashedKey = 0;
+    for(var i = 0; i < key.length; i++){
+      hashedKey = key.charCodeAt(i);
+    }
+    return hashedKey % size;
+  }
+
+  insert(key, value){
+    let index = this.hash(key, this.size);
+    this.buckets[index].set(key, value);
+  }
+
+  remove(key){
+    let index = this.hash(key, this.size);
+    let deldeletedete = this.buckets[index].get(key);
+    this.buckets[index].delete(key)
+  }
+
+  search(key){
+    let index = this.hash(key, this.size);
+    return this.buckets[index].get(key)
+  }
+
+}
+
+let test = new HashTable(20);
+test.insert("John", "John Value")
+test.insert("Son", "Son Value")
+test.insert("Marl", "Marl Value")
+test.insert("Tonny", "Tonny Value")
+console.log(test)
+test.remove("Marl")
+console.log(test)
+test.search('Son')
