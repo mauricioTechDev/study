38e308b39160fad88a0003d523dad7fa6fd0685b
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-02-22 22:34:06 -0500

    Beautiful-Triplets
    Beautiful-Triplets

---
+++ Beautiful-Triplets.js
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/beautiful-triplets/problem
+
+function beautifulTriplets(d, arr) {
+// [i, j, k]
+// j-i and k - j
+let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.includes(arr[i] + d) && arr.includes(arr[i] + (d * 2))) {
+            result++
+        }
+    }
+    return result;
+
+}
