272c8e840f50404f2f9c1301cd5b9b5c7218a94d
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-02-24 08:29:26 -0500

    Bill-Division
    Bill-Division

---
+++ hacker-rank/Bill-Division.js
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/bon-appetit/problem
+
+function bonAppetit(bill, k, b) {
+    let sum = 0;
+    
+    for(let i = 0; i < bill.length; i++){
+            sum += bill[i];
+    }
+    let diff = (sum - bill[k])/2;
+    
+    if(diff === b){
+        console.log('Bon Appetit')      
+    } else {
+        console.log(b > diff ? b - diff : diff - b);
+    }
+    
+}
