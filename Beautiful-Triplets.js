4e2ed5a9701ca99c2090e9cfb64a6ec9bff76b26
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-04-11 20:07:52 -0400

    Update Beautiful-Triplets.js

--- Beautiful-Triplets.js
+++ Beautiful-Triplets.js
@@ -1,8 +1,6 @@
 // https://www.hackerrank.com/challenges/beautiful-triplets/problem
 
 function beautifulTriplets(d, arr) {
-// [i, j, k]
-// j-i and k - j
 let result = 0;
 
     for (let i = 0; i < arr.length; i++) {
