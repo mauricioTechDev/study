827c7d7edf9624ff1d30baffc66ff655fbb9b78a
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-04-17 17:08:46 -0400

    will-you-make-it
    will-you-make-it

---
+++ code-wars/will-you-make-it.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump <= fuelLeft * mpg
+};
+
