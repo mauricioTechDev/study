9e1df0aef2ee522975d078fdf7e75c1453fe7e8b
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-04-30 22:05:49 -0400

    Update Bill-Division.js

--- hacker-rank/Bill-Division.js
+++ hacker-rank/Bill-Division.js
@@ -7,11 +7,9 @@ function bonAppetit(bill, k, b) {
             sum += bill[i];
     }
     let diff = (sum - bill[k])/2;
-    
     if(diff === b){
         console.log('Bon Appetit')      
     } else {
         console.log(b > diff ? b - diff : diff - b);
-    }
-    
+    }   
 }
