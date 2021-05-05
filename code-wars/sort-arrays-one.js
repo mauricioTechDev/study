0b24f50b4f5e8fb1bd4ec4841a137ff7f7572aa6
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-05-04 21:35:33 -0400

    sort-arrays-one
    sort-arrays-one

---
+++ code-wars/sort-arrays-one.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+  return names.sort();
+}
