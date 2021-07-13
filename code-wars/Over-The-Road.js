e0ac64d3fc78b97a1c926ee15399191353b569a4
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-07-13 15:18:28 -0400

    Over-The-Road
    Over-The-Road

---
+++ code-wars/Over-The-Road.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/javascript
+
+function overTheRoad(address, n){
+  //code here
+  let total = 2*n;
+  let difference = address-1;
+  return total-difference;
+}
