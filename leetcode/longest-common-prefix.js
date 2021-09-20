e3bcdcea9c9cc01e23665a06abba7703bfc4693a
Mauricio Acosta <mauricio.acosta92@gmail.com>
2021-09-19 23:04:02 -0400

    longest-common-prefix.js

---
+++ leetcode/longest-common-prefix.js
@@ -0,0 +1,24 @@
+
+ * @param {string[]} strs
+ * @return {string}
+ */
+// https://leetcode.com/problems/longest-common-prefix/
+var longestCommonPrefix = function(strs) {
+    const emptyIndex = strs.findIndex(item => item === "");
+    if (strs.length === 0 || emptyIndex !== -1) {
+      return "";
+    }
+    let commonFirstChar = "";
+    for(i = 0; i < strs.length; i ++) {
+      const firstChar = strs[i].substring(0, 1);
+      if (commonFirstChar === "") {
+        commonFirstChar = firstChar;
+      } else {
+        if (firstChar !== commonFirstChar) {
+          return "";
+        }
+      }
+    }
+    const newStrs = strs.map(item => item.substring(1));
+    return commonFirstChar + longestCommonPrefix(newStrs);
+};
