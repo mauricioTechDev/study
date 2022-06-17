2c90bb6e209c53b4d926ff92af81bd71c68da5f3
Mauricio Acosta <mauricio.acosta92@gmail.com>
2022-06-17 14:15:04 -0400

    reverse-linked-list

---
+++ neetcode/blind-75/reverse-linked-list.js
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    let nextVal = null
+    
+    while(curr !== null){
+        nextVal = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nextVal
+    }
+    
+    return prev
+
+}
+
