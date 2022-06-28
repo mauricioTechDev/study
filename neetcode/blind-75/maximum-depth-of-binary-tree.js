ac8421c6124a3cff3b5e87e44cabd8e3fd2ac28b
Mauricio Acosta <mauricio.acosta92@gmail.com>
2022-06-28 15:08:28 -0400

    Breadth first search Solution

--- neetcode/blind-75/maximum-depth-of-binary-tree.js
+++ neetcode/blind-75/maximum-depth-of-binary-tree.js
@@ -12,6 +12,8 @@ https://leetcode.com/problems/maximum-depth-of-binary-tree/
  * @param {TreeNode} root
  * @return {number}
  */
+
+//////Recusrise Solution
 var maxDepth = function(root) {
     // If the root is null return zero
     if(root == null) return 0;
@@ -19,4 +21,29 @@ var maxDepth = function(root) {
 //     We do plus 1 becouse there is a least a head node 
     let res = 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
     return res
+};
+
+/////Breadth first search Solution
+var maxDepth = function(root) {
+    // If the root is null return zero
+    if(root == null) return 0;
+    
+    let q = [root];
+    let level = 0;
+    
+    while(q.length !== 0){
+        let len = q.length;
+        while(len !== 0){
+            let currentNode = q.shift()
+            if(currentNode.left){
+                q.push(currentNode.left)
+            }
+            if(currentNode.right){
+                q.push(currentNode.right)
+            }
+            len-=1
+        }
+        level+=1
+    }
+    return level
 };
